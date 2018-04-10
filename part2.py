03db247376e77b2b976b37089b16b7cb53384b47
FedorYatsenko <f.yatsenko@knu.ua>
2018-04-10 19:58:07 +0300

    Update: додано print додаткової інформації на екран

--- part2.py
+++ part2.py
@@ -31,10 +31,42 @@ def devide(dividend, divisor):
     return quotient
 
 
+def devide_with_shift(dividend, divisor):
+    res = 0
+
+    remainder = dividend << 1
+    print("Init remainder = %s" % format(remainder, 'b'))
+
+    for i in range(0, dividend.bit_length()):
+        res = res << 1
+        remainder -= divisor << dividend.bit_length()
+
+        spc("#")
+        print("remainder = remainder – divisor = %s" % format(remainder, 'b'))
+
+        if remainder < 0:
+            remainder += divisor << dividend.bit_length()
+            remainder = remainder << 1
+            print("remainder < 0: remainder -> + divisor, shift 1; res -> shift")
+        else:
+            remainder = remainder << 1
+            remainder += 1
+            print("remainder >= 0: remainder -> shift 1, + 1; res -> +1, shift")
+            res += 1
+
+        print("remainder = %s" % format(remainder, 'b'))
+        print("res = %s" % format(res, 'b'))
+
+    return res
+
+
 main_dividend = int(input("Input dividend: "), 2)
 main_divisor = int(input("Input divisor: "), 2)
+spc("#")
 
-res = devide(main_dividend, main_divisor)
+res = devide_with_shift(main_dividend, main_divisor)
 
 spc("#")
-print("Quotient: ", "  %20s (" % format(res, 'b'),res, ")")
+print("%s (" % format(main_dividend, 'b'), main_dividend,
+      ") /  %s (" % format(main_divisor, 'b'), main_divisor,
+      ") =  %s (" % format(res, 'b'), res, ")")
